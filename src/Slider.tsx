6498e1ef8b5fd0aec000d50cfbffc3609c4c7890
plumSemPy <emin@berkeley.edu>
2019-11-13 22:48:29 -0800

    set up draggable mvp, wip: flexbox the svgs so it looks good

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,29 +1,53 @@
-import React, {Dispatch, MouseEvent, SetStateAction, useState} from "react";
+import React, { Dispatch, MouseEvent, SetStateAction, useState } from "react";
 
-const handleDrag = (event:MouseEvent, setCx:Dispatch<SetStateAction<string>>) => {
-    
-    // don't know how to fix this
-    const onMouseMove = (event:any) => {
-        console.log(event)
-       
-    }
+const onMouseDown = (
+  event: MouseEvent,
+  setCx: Dispatch<SetStateAction<string>>
+) => {
+  event.preventDefault();
+  console.log("clicked!");
 
-    document.addEventListener('mousemove', onMouseMove);
-    // document.addEventListener('mouseup', onMouseUp);
+  // don't know how to fix this
+  const onMouseMove = (event: any) => {
+    console.log(event);
+  };
 
-}
+  const onMouseUp = (event: any) => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  };
 
-const Slider: React.FC = () =>{
-    let [cx, setCx] = useState("5") 
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+};
 
-    return (
-       <svg width="300" height="10">
-        <circle cx={cx} cy="5.5" r="4" />
-        <rect y="5" width="300" height="2" rx="10" ry="10" style={{fill: "rgb(96,125,139)"}}
-            onClick={ (e:MouseEvent) => handleDrag(e, setCx)} 
-        />  
-       </svg> 
-    )
-}
+const Slider: React.FC = () => {
+  let [cx, setCx] = useState("5");
+
+  return (
+    <div>
+      <svg width="300" height="10">
+        <rect
+          y="5"
+          width="300"
+          height="2"
+          rx="10"
+          ry="10"
+          style={{ fill: "rgb(96,125,139)" }}
+        />
+      </svg>
+      <div draggable={true} onDrag={() => console.log("boob")}>
+        <svg width="10" height="10">
+          <circle
+            cx={cx}
+            cy="0"
+            r="4"
+            onDrag={(e: MouseEvent) => console.log("draggedme")}
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export default Slider;
--- src/row.tsx
+++ src/row.tsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Tablet from "./Tablet";
 
-interface LooseObject{
+interface LooseObject {
   [key: string]: string;
 }
 
-const Row: React.FC<{ rowName: string, row: LooseObject }> = ({rowName, row}) =>{
-  let [name, setName] = useState(rowName)
-    
-   return (
+const Row: React.FC<{ rowName: string; row: LooseObject }> = ({
+  rowName,
+  row
+}) => {
+  let [name, setName] = useState(rowName);
+
+  return (
     <div></div>
     // playing with row name col name of this hook thing
-       // not sure how much i need a database given hooks
-   ) 
-}
+    // not sure how much i need a database given hooks
+  );
+};
